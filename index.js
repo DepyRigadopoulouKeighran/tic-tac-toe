71fbfe3518f14ed6e6f99d20878e1242170f8349
Despoina Rigadopoulou-Keighran <lightblue@duck.com>
2024-06-05 21:37:33 +0200

    positioning changes

--- index.js
+++ index.js
@@ -7,8 +7,8 @@ const boardState = Array(tiles.length);
 boardState.fill(null);
 
 const strike = document.getElementById("strike");
-const gameOverArea = document.getElementById("game-over-area");
-const gameOverText = document.getElementById("game-over-text");
+const gameOverBox = document.getElementById("game-over-box");
+const gameOverMessage = document.getElementById("game-over-message");
 const playAgain = document.getElementById("play-again");
 playAgain.addEventListener("click", startNewGame);
 
@@ -35,7 +35,7 @@ function setHoverText() {
 setHoverText();
 
 function tileClick(event) {
-  if (gameOverArea.classList.contains("visible")) {
+  if (gameOverBox.classList.contains("visible")) {
     return;
   }
 
@@ -89,14 +89,14 @@ function gameOverScreen(winnerText) {
   if (winnerText != null) {
     text = `Winner is ${winnerText}!`;
   }
-  gameOverArea.className = "visible";
-  gameOverText.innerText = text;
+  gameOverBox.className = "visible";
+  gameOverMessage.innerText = text;
   gameOverSound.play();
 }
 
 function startNewGame() {
   strike.className = "strike";
-  gameOverArea.className = "hidden";
+  gameOverBox.className = "hidden";
   boardState.fill(null);
   tiles.forEach((tile) => (tile.innerText = ""));
   turn = PLAYER_X;
@@ -107,11 +107,11 @@ const winningCombinations = [
   { combo: [1, 2, 3], strikeClass: "strike-row-1" },
   { combo: [4, 5, 6], strikeClass: "strike-row-2" },
   { combo: [7, 8, 9], strikeClass: "strike-row-3" },
-  //columns
+
   { combo: [1, 4, 7], strikeClass: "strike-column-1" },
   { combo: [2, 5, 8], strikeClass: "strike-column-2" },
   { combo: [3, 6, 9], strikeClass: "strike-column-3" },
-  //diagonals
+
   { combo: [1, 5, 9], strikeClass: "strike-diagonal-1" },
   { combo: [3, 5, 7], strikeClass: "strike-diagonal-2" },
 ];
